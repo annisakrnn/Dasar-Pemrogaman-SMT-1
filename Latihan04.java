663eaa3e4d6b76a2afae16a78f29d13507c5f0c1
Annisa Kurniawati <annisak0708@gmail.com>
2023-09-06 12:57:09 +0700

    first commit

---
+++ Latihan04.java
@@ -0,0 +1,11 @@
+/**
+ * Latihan04
+ */
+public class Latihan04 {
+
+    public static void main(String[] args) {
+        System.out.print("Annisa Kurniawati");
+        System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi POLINEMA");
+    
+    }
+}
---
+++ Tugas25.java
@@ -0,0 +1,13 @@
+/**
+ * Tugas25
+ */
+public class Tugas25 {
+public static void main(String[] args) {
+    System.out.println("***** IDENTITAS DIRI*****") ;
+    System.out.println("Nama       : Annisa Kurniawati") ;
+    System.out.println("Absen      : 03") ;
+    System.out.println("Kelas      : 1F") ;
+    System.out.println("Prodi      : Teknik Informatika") ;
+    System.out.println("Jurusan    : teknologi Informasi") ;
+}
+}
